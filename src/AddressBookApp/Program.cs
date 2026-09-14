f8e19922be487a23f34ec485c25e9f396f1601e9
sneha <snehagaba233@gmail.com>
2026-09-14 12:33:49 +0530

    UC1:create contact

--- src/AddressBookApp/Program.cs
+++ src/AddressBookApp/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using AddressBookApp.Models;
+
+namespace AddressBookApp;
+class Program
+{
+    static void Main()
+    {
+        Contact contact = new Contact(
+            "Sneha",
+            "Gaba",
+            "12 Sector-9",
+            "Ambala",
+            "Haryana",
+            "134003",
+            "9876543210",
+            "snehagaba233@mail.com"
+        );
+
+        Console.WriteLine(contact.ToString());
+    }
+}
